4ddf6d12e380c6d76514ae7da77f90914dffb9c0
kost90 <kostdonc90@gmail.com>
2024-01-06 14:02:01 +0000

    edited picture in wellcome section, added svg DM to the wellcome section, edited styles in wellcome section

--- src/ui/wellcomsec/Wellsection.tsx
+++ src/ui/wellcomsec/Wellsection.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import MariaPhoto from "../../../public/pictures/Maria.jpeg";
+import MariaPhoto from "../../../public/pictures/maria_withou_bg.png";
+import DM from "../../../public/svg/DM.svg";
 import styles from "./Wellsection.module.css";
 import Button from "@/components/button/Button";
 
 function Wellsection() {
   return (
-    <div className="flex flex-col md:flex-row items-center md:justify-around gap-6">
+    <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-around gap-6 m-auto">
       <div className="flex flex-col gap-5 lg:gap-8 md:w-text w-80">
         <h1 className="text-Heading3 md:text-Heding2 lg:text-Heding1 font-semibold leading-6 md:leading-10">
           Hi There, I'm
@@ -23,12 +24,14 @@ function Wellsection() {
           className={styles.button_contatc}
         />
       </div>
-      <Image
-        src={MariaPhoto}
-        alt="Maria_photo"
-        width={500}
-        className={styles.photo_maria}
-      />
+      <div className="relative">
+        <Image src={DM} alt="svg_picture" className={styles.svg_dm} />
+        <Image
+          src={MariaPhoto}
+          alt="Maria_photo"
+          className={styles.photo_maria}
+        />
+      </div>
     </div>
   );
 }
